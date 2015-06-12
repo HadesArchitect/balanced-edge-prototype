a0a845f04161e18055b67509832a93bc6e1bb832
Aleksandr Volochnev <a.volochnev@gmail.com>
2015-06-13 01:59:02 +0300

    App improved

--- app/index.php
+++ app/index.php
@@ -1,1 +1,13 @@
-<?php echo gethostname(), '_________ ',date('d.m.Y h:i:s');
+<?php 
+
+    header('Content-Type: image/jpeg');
+    header('X-APP-HOSTNAME: ' . gethostname());
+    header('X-APP-TIME: ' . date('d.m.Y H:i:s'));
+
+    $img   = ImageCreateFromJPEG('cat.jpg');
+    $color = imagecolorallocate($img, 0, 0, 0);
+
+    imagestring($img, 5, 146, 22, 'Host: ' . gethostname(), $color);
+    imagestring($img, 5, 146, 36, date('d.m.Y H:i:s'), $color);
+
+    imagejpeg($img);
